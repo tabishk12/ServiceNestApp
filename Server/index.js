f518eb1a4b55fb06c95faf4caf1fc3f14308fa6c
tabish khan <137671926+tabishk12@users.noreply.github.com>
2025-09-27 12:18:35 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -17,10 +17,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "*",
+  origin: "https://servicenestapp.onrender.com" && "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
 app.use(express.json()); 
 app.use(cookieParser());
 // Routes
