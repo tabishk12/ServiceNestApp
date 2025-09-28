ca8da80d4f24a9166abc4ae146a5641efbb82f20
tabish khan <137671926+tabishk12@users.noreply.github.com>
2025-09-28 11:45:20 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
